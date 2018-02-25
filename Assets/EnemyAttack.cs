142794fc22e3cfe436de5d32a74f2a9dd129f573
johnklima <klima.de.sa@gmail.com>
2018-02-25 15:51:46 +0100

    first commit

---
+++ Assets/EnemyAttack.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
+    public int attackDamage = 10;               // The amount of health taken away per attack.
+
+
+    Animator anim;                              // Reference to the animator component.
+    GameObject player;                          // Reference to the player GameObject.
+    bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+    float timer;                                // Timer for counting up to the next attack.
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is in range.
+            playerInRange = true;
+            Debug.Log("player in range");            
+            anim.SetTrigger("Swing");
+
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        // If the exiting collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is no longer in range.
+            playerInRange = false;
+            Debug.Log("player exit range");
+        }
+    }
+
+
+    void Update()
+    {
+        // Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+        // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+        if (timer >= timeBetweenAttacks && playerInRange )
+        {
+            // ... attack.
+            Attack();
+        }
+
+        
+    }
+
+
+    void Attack()
+    {
+        // Reset the timer.
+        timer = 0f;
+        
+    }
+}
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;    //important!!!
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+    Animator anim;                  // Reference to the animator component.
+
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        // ... set the destination of the nav mesh agent to the player.
+        nav.SetDestination(player.position);
+        anim.SetTrigger("MoveToPlayer");
+        
+    }
+}
